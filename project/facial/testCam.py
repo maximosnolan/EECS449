deddb0f9825c5c2f85202646928127f3666eab54
Zachary Goldston <zwgold@umich.edu>
2023-03-23 13:54:04 -0400

    testing camera script fo anyone who needs it to figure out cam port

---
+++ project/facial/testCam.py
@@ -0,0 +1,17 @@
+import cv2
+
+if __name__ == '__main__':
+    cam_port = 0
+    cam = cv2.VideoCapture(cam_port)
+    
+    result, image = cam.read()
+    
+
+    if result:
+        cv2.imshow("test", image)
+        cv2.imwrite("test.png", image)
+        cv2.waitKey(0)
+        cv2.destroyWindow("test")
+    
+    else:
+        print("No image detected. Please! try again")
